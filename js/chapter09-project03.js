55d9fc7b226132a735dcc2110a957469991d6620
arsh1401 <59576728+arsh1401@users.noreply.github.com>
2020-12-07 20:29:40 -0500

    .js file
    updated

--- js/chapter09-project03.js
+++ js/chapter09-project03.js
@@ -4,6 +4,7 @@ function loadEvent()
 {
 document.getElementById("highlight").style.display = "block";
 document.getElementById("hide").style.display = "none";
+
 document.getElementById("highlight").addEventListener("click", function(){
 
 var elements = document.querySelectorAll("h1, main, div, p, tr, td, caption, th, a, legend, label, fieldset, button");
@@ -14,29 +15,33 @@ var spanEle = document.createElement('SPAN');
 spanEle.className = "hoverNode";
 spanEle.innerHTML = elements[i].tagName;
 elements[i].appendChild(spanEle);
-
 spanEle.addEventListener("click", function(e){
-    var parentElement = e.currentTarget.parentElement;
-    var parentId = parentElement.getAttribute("id");
-    var tagName = parentElement.tagName;
-    var className = parentElement.className;
-    var innerhtml = parentElement.innerHTML;
-    var alertMsg = parentId + " " + tagName + " " + className + " " + innerhtml;
-    alert(alertMsg);
-    });
+var parentElement = e.currentTarget.parentElement;
+var parentId = parentElement.getAttribute("id");
+var tagName = parentElement.tagName;
+var className = parentElement.className;
+var innerhtml = parentElement.innerHTML;
+var alertMsg = parentId + " " + tagName + " " + className + " " + innerhtml;
+alert(alertMsg);
+});
 }
 }
+
+document.getElementById("highlight").style.display = "none";
+document.getElementById("hide").style.display = "block";
 
 });
-
 document.getElementById("hide").addEventListener("click", function(){
-    while(document.getElementsByClassName("hoverNode").length > 0)
-    {
-    var spanNodes = document.getElementsByClassName("hoverNode");
-    for (var i = 0; i < spanNodes.length; i++) {
-    spanNodes[i].remove();
-    }
-    }
+while(document.getElementsByClassName("hoverNode").length > 0)
+{
+var spanNodes = document.getElementsByClassName("hoverNode");
+for (var i = 0; i < spanNodes.length; i++) {
+spanNodes[i].remove();
+}
+}
+
+document.getElementById("highlight").style.display = "block";
+document.getElementById("hide").style.display = "none";
 });
 
 }
